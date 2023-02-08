b255e6e70f4c7c5a58f13c7f66ae5174016f44aa
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-02-08 17:15:50 -0300

    Adiciona Botões

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -4,7 +4,10 @@ main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
 
-  // const PerguntaApp({super.key});
+  final perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o se animal favorito?'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +17,14 @@ class PerguntaApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(onPressed: null, child: Text('Resposta 1')),
+            ElevatedButton(onPressed: null, child: Text('Resposta 2')),
+            ElevatedButton(onPressed: null, child: Text('Resposta 3')),
+          ],
+        ),
       )
     );
   }
