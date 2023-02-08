51dc52a5be337b0b443396b26908a3b042165e1b
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-02-08 17:15:55 -0300

    Cria o widget Questionario (Refactory) (61)

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/questionario.dart';
 import 'package:projeto_perguntas/resultado.dart';
-import './questao.dart';
-import './resposta.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -42,21 +41,16 @@ class _PerguntaAppState extends State<PerguntaApp> {
   @override
   Widget build(BuildContext context) {
 
-    List<String> respostas = temPerguntaSelecionada
-      ? _perguntas[_perguntaSelecionada].cast()['respostas']
-      : [];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
         body: temPerguntaSelecionada
-          ? Column(
-              children: [
-                Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
-                ...respostas.map((resposta) => Resposta(resposta, _responder)).toList()
-              ],
+          ? Questionario(
+              perguntas: _perguntas,
+              perguntaSelecionada: _perguntaSelecionada,
+              responder: _responder,
             )
           : const Resultado(),
       )
---
+++ projeto_perguntas/lib/questionario.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/questao.dart';
+import 'package:projeto_perguntas/resposta.dart';
+
+class Questionario extends StatelessWidget {
+  final List<Map<String, Object>> perguntas;
+  final int perguntaSelecionada;
+  final void Function() responder;
+
+  const Questionario({
+    super.key,
+    required this.perguntas,
+    required this.perguntaSelecionada,
+    required this.responder,
+  });
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada].cast()['respostas']
+        : [];
+
+    return Column(
+      children: [
+        Questao(perguntas[perguntaSelecionada]['texto'].toString()),
+        ...respostas.map((resposta) => Resposta(resposta, responder)).toList()
+      ],
+    );
+  }
+}
