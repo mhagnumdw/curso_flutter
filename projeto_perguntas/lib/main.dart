68500330c9ee55fc24e3f409118d693f89e1eb49
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-02-08 17:15:53 -0300

    Passando Função Callback para o widget Resposta

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -22,7 +22,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
   Widget build(BuildContext context) {
     final perguntas = [
       'Qual é a sua cor favorita?',
-      'Qual é o se animal favorito?'
+      'Qual é o seu animal favorito?'
     ];
 
     return MaterialApp(
@@ -34,9 +34,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1'),
-            Resposta('Resposta 2'),
-            Resposta('Resposta 3'),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),
       )
--- projeto_perguntas/lib/resposta.dart
+++ projeto_perguntas/lib/resposta.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
+
   final String resposta;
 
-  // const Reposta({super.key, required this.resposta});
-  const Resposta(this.resposta);
+  final void Function() quandoSelecionado;
+
+  const Resposta(this.resposta, this.quandoSelecionado, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: null,
-      child: Text(resposta),
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: quandoSelecionado,
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+        child: Text(
+          resposta,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
