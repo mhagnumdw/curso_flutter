2fe0d80a47e51cb155a3182067fe1c2b24c058ba
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-02-08 17:15:50 -0300

    Adiciona uma barra no topo

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -9,7 +9,13 @@ class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Olá Flutter!!!'),
+      // home: Text('Olá Flutter!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter!!!'),
+      )
     );
   }
 
