bb8380aa5dbbb01e910c9c2f880b30b36e5393bc
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-02-23 13:50:43 -0300

    [expenses] 96. Desafio Estilo Título & Data - Resposta

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -63,11 +63,10 @@ class MyHomePage extends StatelessWidget {
                         vertical: 10,
                       ),
                       decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )
-                      ),
+                          border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )),
                       padding: EdgeInsets.all(10),
                       child: Text(
                         tr.value.toString(),
@@ -79,9 +78,21 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(tr.title),
-                        Text(tr.date.toString()),
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
                       ],
                     )
                   ],
