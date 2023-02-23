1d729f605a17fdfeecc0710f29b8ef312fac8a79
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-02-23 14:29:39 -0300

    [expenses] 100. Instalando Pacote Externo Intl

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 // void main(List<String> args) {
 //   runApp(const ExpensesApp());
@@ -88,7 +89,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
                           style: const TextStyle(
                             color: Colors.grey,
                           ),
