8aaaa91773907095bda195c806cb850fb132ae54
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-03-13 13:36:01 -0300

    [expenses] 107. Usando ListView

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -10,10 +10,11 @@ class TransactionLint extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
+        height: 300,
+        child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            final tr = transactions[index];
             return Card(
               child: Row(
                 children: [
@@ -58,9 +59,7 @@ class TransactionLint extends StatelessWidget {
                 ],
               ),
             );
-          }).toList(),
-        ),
-      ),
-    );
+          },
+        ));
   }
 }
